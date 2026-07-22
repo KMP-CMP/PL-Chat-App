ed5689306cbb3fb7c4d25239a592b5ba9bccb433
윤찬 <vsvx13@naver.com>
2026-07-22 16:42:12 +0900

    [feat] 공통 버튼 컴포넌트 구현 (#11)

--- build-logic/convention/src/main/kotlin/CmpLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CmpLibraryConventionPlugin.kt
@@ -19,6 +19,13 @@ class CmpLibraryConventionPlugin : Plugin<Project> {
                 "commonMainImplementation"(libs.findLibrary("jetbrains-compose-material-icons-core").get())
                 "commonMainImplementation"(libs.findLibrary("jetbrains-compose-ui").get())
                 "commonMainImplementation"(libs.findLibrary("jetbrains-compose-foundation").get())
+
+                // commonMain에는 @Preview 선언 API를 제공하고, Android 렌더러는 런타임 classpath에만 둡니다.
+                // Android-KMP의 단일 variant에서도 tooling을 컴파일·배포 API에 노출하지 않고 Preview를 실행할 수 있습니다.
+                // https://developer.android.com/kotlin/multiplatform/plugin#add-compose-preview-dependencies
+                "commonMainImplementation"(libs.findLibrary("jetbrains-compose-ui-tooling-preview").get())
+
+                "androidRuntimeClasspath"(libs.findLibrary("androidx-compose-ui-tooling").get())
             }
         }
     }
